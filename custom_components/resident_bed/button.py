81eaab1591a67039b3d7d182587266991ed8fbff
Tyler Hartwig <tyler.hartwig@parivedasolutions.com>
2025-02-27 18:39:32 -0500

    Add logging for characteristics

--- custom_components/resident_bed/button.py
+++ custom_components/resident_bed/button.py
@@ -52,6 +52,10 @@ class ResidentBedButton(ResidentBedEntity):
         ble_device = bluetooth.async_ble_device_from_address(self.hass, self.mac, connectable=True)
         async with BleakClient(ble_device) as client:
             await client.connect()
+            service_collection = self.bleak_client.services
+            characteristics = service_collection.characteristics
+            _LOGGER.info(f"Service collection: {service_collection}")
+            _LOGGER.info(f"characteristics: {characteristics}")
             await client.write_gatt_char(
                 WRITE_CONTROL_HANDLE,
                 binascii.a2b_hex(self.command.value), response=True)
